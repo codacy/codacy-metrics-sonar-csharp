708a1a607e4242a5b7b15178e7e05f3f82c72425
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-03-16 15:10:36 +0100

    clean: Use TIMEOUT_SECONDS env variable

--- src/Seed/TimeSpanHelper.cs
+++ src/Seed/TimeSpanHelper.cs
@@ -2,42 +2,22 @@ using System;
 
 namespace CodacyCSharp.Metrics.Seed
 {
+    /// <summary>
+    ///     TimeSpan helper.
+    ///     This parse a given string to a TimeSpan.
+    /// </summary>
     public static class TimeSpanHelper
     {
+        /// <summary>
+        ///     Parse a TimeSpan from a string that contains a integer
+        ///     with seconds
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
         public static TimeSpan Parse(string value)
         {
-            var timeSplitted = value.Replace('.', ' ').Split(' ');
-            int timeFactor;
-
-            if (timeSplitted.Length > 1)
-            {
-                switch (timeSplitted[1])
-                {
-                    case "second":
-                    case "seconds":
-                        timeFactor = 1;
-                        break;
-
-                    case "minute":
-                    case "minutes":
-                        timeFactor = 60;
-                        break;
-
-                    case "hour":
-                    case "hours":
-                        timeFactor = 3600;
-                        break;
-
-                    default:
-                        throw new FormatException();
-                }
-            }
-            else
-            {
-                timeFactor = 1;
-            }
-
-            return TimeSpan.FromSeconds(int.Parse(timeSplitted[0]) * timeFactor);
+            return TimeSpan.FromSeconds(int.Parse(value));
         }
     }
 }
